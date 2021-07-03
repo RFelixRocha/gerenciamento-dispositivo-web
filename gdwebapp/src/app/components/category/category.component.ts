bc59ed98cebb6dbd496ee512cf125a163e73468f
RFelixRocha <felixrochaeso@gmail.com>
2021-07-03 06:40:05 -0400

    Listagem de categorias

--- gdwebapp/src/app/components/category/category.component.ts
+++ gdwebapp/src/app/components/category/category.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from "../../interfaces/category";
+import { CategoryService } from "../../services/category.service";
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = []
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(): void {
+      this.categoryService.getCategories().subscribe(categories => this.categories = categories)
   }
 
 }
--- gdwebapp/src/app/services/category.service.ts
+++ gdwebapp/src/app/services/category.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+import { Category } from "../interfaces/category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  private categoryUrl = 'http://localhost:3000/api/v1/categories';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-type': 'application/json'})
+  }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  /**  Busca todas as categorias */
+  getCategories(): Observable<Category[]> {
+    const url = `${this.categoryUrl}/all`;
+    return this.http.get<Category[]>(url)
+      .pipe(
+        tap(_=> this.log('fetched category')),
+        catchError(this.handleError<Category[]>('getCategories', []))
+      );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log do Category.service */
+  private log(message: string) {
+    console.log(message);
+  }
+
 }
