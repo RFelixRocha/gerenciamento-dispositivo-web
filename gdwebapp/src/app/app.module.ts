ca247952c2aa1e6da269188abf400f4a4fa9f4e1
RFelixRocha <felixrochaeso@gmail.com>
2021-07-03 01:44:46 -0400

    Add Navbar

--- gdwebapp/src/app/app.module.ts
+++ gdwebapp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './components/device/device.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
