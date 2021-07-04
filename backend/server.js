ee63829ab373b3ac43a72fea78e3d1ff759fd908
RFelixRocha <felixrochaeso@gmail.com>
2021-07-03 23:08:00 -0400

    Add package dotenv e padronização da estrutura de rotas.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app  = express();
 const db   = require("./models");
-const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 
+dotenv.config();
 app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 /** parse requests of content-type - application/json */
 app.use(bodyParser.json());
@@ -13,8 +16,12 @@ app.use(bodyParser.json());
 /** parse requests of content-type - application/x-www-form-urlencoded */
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+/** Pra verificar se a api está em execução */
+const index = require("./routes/index");
+app.use(index);
 
 const deviceRoutes = require("./routes/device.routes");
 app.use("/api/v1/devices",deviceRoutes);
