7641ae7f1bc0e0bd475de0c15696a65e5a7fd680
RFelixRocha <felixrochaeso@gmail.com>
2021-07-05 14:03:16 -0400

    Ajustes na rota de pesquisa de dispositivos.

--- backend/controllers/device.controller.js
+++ backend/controllers/device.controller.js
@@ -77,25 +77,32 @@ exports.update = (req, res) => {
   };
 
   //Busca um dispositivo
-  exports.findOne = (req, res) => {
+    exports.findOne = (req, res) => {
 
-    const id = req.params.id;
+        const id = req.params.id;
 
-    DeviceController.findAll({
-      where:{ id:id },
-      include: db.category
-     })
-    .then(data => {
-      res.status(200).send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Erro ao buscar o dispositivo."
-      });
-    });
+        DeviceController.findOne({
+            where:{ id:id },
+            include:[db.category]
+        })
+            .then(data => {
 
-  };
+                if(data == null)
+                {
+                    res.status(404).send({
+                        message: "Registro não encontrado."
+                    });
+
+                }else res.status(200).send(data);
+
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: "Erro ao buscar o dispositivo com o id=" + id
+                });
+            });
+
+    };
 
 
   // Deleta um dispositivo
